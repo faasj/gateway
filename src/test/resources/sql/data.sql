d14d21988becdd557b865bc1b1c44459bce129b7
chesnokov-aa <alesha.chesnokov@gmail.com>
2021-06-20 20:42:53 +0300

    добавил использование crud методов для класса Project

--- src/test/resources/sql/data.sql
+++ src/test/resources/sql/data.sql
@@ -13,3 +13,19 @@ values (
     '{"limits": "val", "limits1": "val1"}',
     '{"requests": "request", "requests1": "request1"}'
 );
+insert into projects
+values (
+  '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+  'calc',
+  '2021-06-20 18:00:00',
+  '2021-06-20 18:00:00',
+  '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+  '{"additionalProp1": "1string", "additionalProp2": "2string", "additionalProp3": "3string"}'
+    );
+
+--        private UUID projectId;
+--        private String projectName;
+--        private LocalDateTime createDate;
+--        private LocalDateTime updateDate;
+--        private UUID ownerId;
+--        private Map<String, String> tags;
