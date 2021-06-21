860734553bc6567c84908164777dd11fa5eb363d
Semibratov Ilya <i7brothers@gmail.com>
2021-06-21 22:18:09 +0400

    added DeployServiceImpl and DeployDto classes

---
+++ src/test/java/com/faasj/gateway/service/DeployServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.faasj.gateway.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeployServiceImplTest {
+
+    @Test
+    void deployFunction() {
+    }
+
+    @Test
+    void findInstance() {
+    }
+}
